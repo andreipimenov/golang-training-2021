40fe9146734ec306fdb5f474b0dbf5c6a0c96d31
3xwr <medic8070@gmail.com>
2021-07-01 20:26:23 +0300

    fixed an error

--- 01_intro/homework/main.go
+++ 01_intro/homework/main.go
@@ -5,12 +5,12 @@ import (
 	"math"
 )
 
-func sphereVolume(r int) float64 {
-	return math.Pi * 4/3
+func sphereVolume(r float64) float64 {
+	return math.Pow(r,3)*math.Pi * 4/3
 }
 
 func main() {
-	var r int
+	var r float64
 	r = 6
-	fmt.Printf("The volume of a sphere with radius %d is %f",r, sphereVolume(r))
+	fmt.Printf("The volume of a sphere with radius %f is %f",r, sphereVolume(r))
 }
