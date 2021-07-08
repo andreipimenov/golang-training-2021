0fec04d43ea94d013bbb2463eedee3f6c6fd6aaa
Andrei Pimenov <25442973+andreipimenov@users.noreply.github.com>
2021-07-08 15:26:00 +0300

    Add 03 lesson notes

---
+++ 03_map_interface_method/05_addressable_value_struct/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Device struct {
+	ID string
+}
+
+func (d *Device) Print() {
+	fmt.Println("Device ID is", d.ID)
+}
+
+func main() {
+	d := Device{"12345"}
+	d.Print()
+
+	ds := map[string]Device{ // change Device to *Device to fix compilation error
+		"first": {"666"},
+	}
+	ds["first"].Print()
+
+	v := []Device{
+		{"888"},
+	}
+	v[0].Print()
+}
