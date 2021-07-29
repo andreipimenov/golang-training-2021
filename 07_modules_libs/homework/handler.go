b53dda066c3c1b631f5027e4b39f4c9b8cc2c89e
AlexanderOhotin <43857584+AlexanderOhotin@users.noreply.github.com>
2021-07-29 15:11:28 +0300

    Add files via upload

--- 07_modules_libs/homework/handler.go
+++ 07_modules_libs/homework/handler.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"io/ioutil"
+	"log"
+	"math"
 	"net/http"
 )
 
@@ -16,30 +18,53 @@ func stockHandler(w http.ResponseWriter, r *http.Request) {
 
 	url := fmt.Sprintf("https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=%s&apikey=%s", ticker, token)
 
+	var highest, lowest, average, total float64
+
 	response, err := http.Get(url)
 	if err != nil {
 		writeResponse(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	body, err2 := ioutil.ReadAll(response.Body)
+	body, err := ioutil.ReadAll(response.Body)
 
-	var s interface{}
+	var s StockRequest
 	err = json.Unmarshal(body, &s)
 	if err != nil {
-		writeResponse(w, http.StatusBadRequest, err.Error())
-		return
-	}
-	if err2 != nil {
-		writeResponse(w, http.StatusBadRequest, err.Error())
+		writeResponse(w, http.StatusBadRequest, err.Error)
 		return
 	}
 
-	writeResponse(w, http.StatusOK, s)
+	highest = s.Price[0].StockPrice
+	lowest = s.Price[0].StockPrice
+
+	for _, i := range s.Price {
+		if i.StockPrice > highest {
+			highest = i.StockPrice
+		}
+		if i.StockPrice < lowest {
+			lowest = i.StockPrice
+		}
+		total += i.StockPrice
+	}
+
+	average = math.Floor((total/float64(len(s.Price)))*100) / 100 // round float to 2 decimal places for accuracy
+
+	res := FormattedResponse{
+		Ticker:        ticker,
+		Highest_price: highest,
+		Lowest_price:  lowest,
+		Avg_price:     average,
+	}
+
+	writeResponse(w, http.StatusOK, res)
 }
 
 func writeResponse(w http.ResponseWriter, code int, v interface{}) {
 	b, _ := json.Marshal(v)
 	w.WriteHeader(code)
-	w.Write(b)
+	_, err := w.Write(b)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- 07_modules_libs/homework/model.go
+++ 07_modules_libs/homework/model.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"strconv"
+)
+
+type tickerInfo struct {
+	StockPrice float64
+}
+
+type StockRequest struct {
+	Price []tickerInfo
+}
+
 type FormattedResponse struct {
 	Ticker        string
 	Highest_price float64
@@ -7,6 +21,34 @@ type FormattedResponse struct {
 	Avg_price     float64
 }
 
-// Should add custom unmarshal
-//func (sr *StockRequest) UnmarshalJSON(b []byte) error {
-//}
+func (sr *StockRequest) UnmarshalJSON(b []byte) error {
+	var f interface{}
+	err := json.Unmarshal(b, &f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rawResponse := f.(map[string]interface{})
+
+	data := rawResponse["Monthly Time Series"].(map[string]interface{})
+
+	var openPrice []tickerInfo
+	var currentDayPrice tickerInfo
+	var parsedData map[string]interface{}
+
+	for _, d := range data {
+		parsedData = d.(map[string]interface{})
+
+		currentDayPrice = tickerInfo{}
+		// we use "open" price only in demo purposes. It`s not accurate data for calculating and highest, lowest values
+		// should parse high and low values separately then calculate average price with ((high + low)/2)
+		currentDayPrice.StockPrice, _ = strconv.ParseFloat(parsedData["1. open"].(string), 64)
+
+		openPrice = append(openPrice, currentDayPrice)
+	}
+
+	sr.Price = openPrice
+
+	return nil
+}
+
