9db05106fbdf457e7c5fb4ffed92b1488829b30f
AlexanderOhotin <43857584+AlexanderOhotin@users.noreply.github.com>
2021-08-03 11:33:10 +0300

    Error handling

---
+++ response.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func writeResponse(w http.ResponseWriter, code int, v interface{}) {
+	b, err := json.Marshal(v)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, err := w.Write([]byte(`{"error":"Internal server error"}`))
+		if err != nil {
+			log.Printf("Write failed: %v", err)
+		}
+		return
+	}
+	w.WriteHeader(code)
+	_, err = w.Write(b)
+	if err != nil {
+		log.Printf("Write failed: %v", err)
+	}
+
+}
