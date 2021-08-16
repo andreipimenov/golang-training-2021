f171c7c36cff3c652a597301decd6c030cb8cd6f
Cyril Kuzmin <xxlaefxx@gmail.com>
2021-08-16 19:06:33 +0300

    fix redis_cache errors

--- internal/repository/redis_cache.go
+++ internal/repository/redis_cache.go
@@ -29,7 +29,7 @@ func (c *RedisCache) Load(key string) (model.Price, bool) {
 		c.logger.Error().Err(err).Msg("error during the getting price from cache")
 		return res, false
 	}
-	json.Unmarshal([]byte(val), &res)
+	err = json.Unmarshal([]byte(val), &res)
 	if err != nil {
 		c.logger.Error().Err(err).Msg("cannot unmarsal redis data")
 	}
@@ -40,7 +40,7 @@ func (c *RedisCache) Store(key string, value model.Price) {
 	ctx := context.Background()
 	jsonVal, err := json.Marshal(value)
 	if err != nil {
-		c.logger.Error().Err(err).Msg("cannot cache data into redis")
+		c.logger.Error().Err(err).Msgf("cannot marshal %T object", value)
 	}
 	err = c.Set(ctx, key, jsonVal, 0).Err()
 	if err != nil {
